84bb5962b3b811c0d53bb369c93fcb24ccc7c150
Konstantin Stepanov <me@kstep.me>
2014-12-07 22:07:45 +0300

    trait for song swap

--- src/queue.rs
+++ src/queue.rs
@@ -8,6 +8,7 @@ extern {
     fn mpd_run_get_queue_song_pos(connection: *mut mpd_connection, pos: libc::c_uint) -> *mut mpd_song;
     fn mpd_run_move_id(connection: *mut mpd_connection, from: libc::c_uint, to: libc::c_uint) -> bool;
     fn mpd_run_swap_id(connection: *mut mpd_connection, id1: libc::c_uint, id2: libc::c_uint) -> bool;
+    fn mpd_run_swap(connection: *mut mpd_connection, pos1: libc::c_uint, pos2: libc::c_uint) -> bool;
 }
 
 pub struct MpdQueue<'a> {
@@ -28,7 +29,7 @@ impl<'a> MpdQueue<'a> {
         }
     }
 
-    pub fn move_to(&mut self, song: &MpdSong, pos: uint) -> MpdResult<()> {
+    pub fn move_to(&mut self, pos: uint, song: &MpdSong) -> MpdResult<()> {
         if unsafe { mpd_run_move_id(self.conn.conn, song.id(), pos as libc::c_uint) } {
             Ok(())
         } else {
@@ -36,11 +37,23 @@ impl<'a> MpdQueue<'a> {
         }
     }
 
-    pub fn swap(&mut self, song1: &MpdSong, song2: &MpdSong) -> MpdResult<()> {
-        if unsafe { mpd_run_swap_id(self.conn.conn, song1.id(), song2.id()) } {
+    pub fn swap<T: ToSongPos>(&mut self, song1: T, song2: T) -> MpdResult<()> {
+        if unsafe { mpd_run_swap(self.conn.conn, song1.song_pos(), song2.song_pos()) } {
             Ok(())
         } else {
             Err(FromConn::from_conn(self.conn).unwrap())
         }
     }
+}
+
+trait ToSongPos {
+    fn song_pos(&self) -> uint;
+}
+
+impl ToSongPos for uint {
+    #[inline] fn song_pos(&self) -> uint { *self }
+}
+
+impl ToSongPos for MpdSong {
+    #[inline] fn song_pos(&self) -> uint { self.pos() }
 }
