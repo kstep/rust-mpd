5cde47ad3596fe248a64ce0874be5739674a7015
Konstantin Stepanov <me@kstep.me>
2015-01-04 14:19:39 +0300

    fix tuple encoding

--- src/utils.rs
+++ src/utils.rs
@@ -72,7 +72,8 @@ impl<'a, I: 'a + Iterator<MpdResult<MpdPair>>> Iterator<MpdResult<MpdPair>> for 
 impl<S: Encoder<E>, E, T1: ForceEncodable<S, E>, T2: ForceEncodable<S, E>> ForceEncodable<S, E> for (T1, T2) {
     #[inline] fn encode(&self, s: &mut S) -> Result<(), E> {
         s.emit_tuple(2, |s| {
-            self.0.encode(s).and_then(|_| self.1.encode(s))
+            self.emit_tuple_arg(0, |s| self.0.encode(s)).and_then(|_|
+            self.emit_tuple_arg(1, |s| self.1.encode(s)))
         })
     }
 }
