b545664b6c9c9512c3fc0cfd897c28b0d6fd304c
Konstantin Stepanov <me@kstep.me>
2015-06-18 19:36:17 +0300

    add benches (WIP)

---
+++ benches/helpers.rs
@@ -0,0 +1,10 @@
+extern crate mpd;
+
+use std::net::TcpStream;
+use std::env;
+
+pub fn connect() -> mpd::Client<TcpStream> {
+    let addr = env::var("MPD_SOCK").unwrap_or("127.0.0.1:6600".to_owned());
+    mpd::Client::connect(&*addr).unwrap()
+}
+
---
+++ benches/options.rs
@@ -0,0 +1,20 @@
+#![feature(test)]
+
+extern crate mpd;
+extern crate time;
+extern crate test;
+extern crate unix_socket;
+
+mod helpers;
+use helpers::connect;
+use time::Duration;
+use test::{Bencher, black_box};
+use unix_socket::UnixStream;
+
+#[bench]
+fn status(b: &mut Bencher) {
+    let mut mpd = mpd::Client::<UnixStream>::new(UnixStream::connect("/var/run/mpd/socket").unwrap()).unwrap();
+    b.iter(|| {
+        black_box(mpd.status());
+    });
+}
