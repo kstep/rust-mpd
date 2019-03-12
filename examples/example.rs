61f8137c8b415baf545ca9d6b069cec8cc89d252
Raphael Nestler <raphael.nestler@gmail.com>
2019-03-12 13:44:41 +0100

    Simplify example

--- examples/example.rs
+++ examples/example.rs
@@ -1,10 +1,9 @@
 extern crate mpd;
 
 use mpd::{Client, Query};
-use std::net::TcpStream;
 
 fn main() {
-    let mut c = Client::new(TcpStream::connect("127.0.0.1:6600").unwrap()).unwrap();
+    let mut c = Client::connect("127.0.0.1:6600").unwrap();
     println!("version: {:?}", c.version);
     println!("status: {:?}", c.status());
     println!("stuff: {:?}", c.find(&Query::new(), (1, 2)));
